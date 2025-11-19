5f4a7bc3b06da4798b3d4fd7508a408eab6913d6
Danylo Vesely <danyloveselyi@gmail.com>
2025-11-19 22:42:48 +0100

    InternalFileSystem node db flash to 80  pages

--- libraries/InternalFileSytem/src/InternalFileSystem.cpp
+++ libraries/InternalFileSytem/src/InternalFileSystem.cpp
@@ -26,12 +26,12 @@
 #include "flash/flash_nrf5x.h"
 
 #ifdef NRF52840_XXAA
-  #define LFS_FLASH_ADDR        0xED000
+  #define LFS_FLASH_ADDR        0x80000
 #else
   #define LFS_FLASH_ADDR        0x6D000
 #endif
 
-#define LFS_FLASH_TOTAL_SIZE  (19*FLASH_NRF52_PAGE_SIZE)
+#define LFS_FLASH_TOTAL_SIZE  (80*FLASH_NRF52_PAGE_SIZE)
 #define LFS_BLOCK_SIZE        128
 
 //--------------------------------------------------------------------+
